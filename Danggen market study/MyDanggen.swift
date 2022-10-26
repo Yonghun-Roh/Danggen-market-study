d4546de209fb0c380c30feaef54a88c06dacae8a
Ro YongHun <mogi3206@naver.com>
2022-10-26 19:14:28 +0900

    로그아웃 코드 추가

--- Danggen market study/MyDanggen.swift
+++ Danggen market study/MyDanggen.swift
@@ -6,7 +6,27 @@
 //
 
 import UIKit
+import Firebase
 
 class MyDanggenViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func logOutPressed(_ sender: Any) {
+        
+        
+        let firebaseAuth = Auth.auth()
+    do {
+      try firebaseAuth.signOut()
+    } catch let signOutError as NSError {
+      print("Error signing out: %@", signOutError)
+    }
+    }
+    
+    
+  
     
 }
